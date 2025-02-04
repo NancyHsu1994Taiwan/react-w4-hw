335c1e7d29a38d6f951390f4bb3701c91092383f
nancy,yun-lin,hsu <67849065+NancyHsu1994Taiwan@users.noreply.github.com>
2025-02-05 07:19:27 +0800

    install bootstrap, axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
 function App() {
-
   return (
-    
-  )
+    <button type="button" className="btn btn-primary">
+      click
+    </button>
+  );
 }
 
-export default App
+export default App;
