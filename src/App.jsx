12c0a98919a75537e5b9d9ea24c685748215e4b4
nancy,yun-lin,hsu <67849065+NancyHsu1994Taiwan@users.noreply.github.com>
2025-02-15 08:31:52 +0800

    調整兩個頁面的props

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
 import { useState } from "react";
-import axios from "axios";
 import LoginPage from "./pages/LoginPgae";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
-  const { VITE_BASE_URL: BASE_URL, VITE_API_BASE: API_BASE } = import.meta.env;
   // 登入相關 -----------------------------
   const [isAuth, setIsAuth] = useState(false);
 
   // 產品相關 --------------------------------
-  const [productList, setProductList] = useState([]);
+  // const [productList, setProductList] = useState([]);
 
-  const getProductList = async () => {
-    const result = await axios.get(
-      `${BASE_URL}/api/${API_BASE}/admin/products`
-    );
-    const products = result.data.products;
+  // const getProductList = async () => {
+  //   const result = await axios.get(
+  //     `${BASE_URL}/api/${API_BASE}/admin/products`
+  //   );
+  //   const products = result.data.products;
 
-    setProductList(products);
-  };
+  //   setProductList(products);
+  // };
 
-  return (
-    <>
-      {isAuth ? (
-        <ProductPage
-          getProductList={getProductList}
-          productList={productList}
-        />
-      ) : (
-        <LoginPage setIsAuth={setIsAuth} getProductList={getProductList} />
-      )}
-    </>
-  );
+  return <>{isAuth ? <ProductPage /> : <LoginPage setIsAuth={setIsAuth} />}</>;
 }
 
 export default App;
