38320dc7e19489a3641716c600fea1f7ba52b023
nancy,yun-lin,hsu <67849065+NancyHsu1994Taiwan@users.noreply.github.com>
2025-02-18 13:12:15 +0800

    fix url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "react-w3-hw" : "/",
+  base: process.env.NODE_ENV === "production" ? "react-w4-hw" : "/",
   plugins: [react()],
 });
